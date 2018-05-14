e1ea18337ec7dced8aa0b936d04cd6a5f8fcb36b
Christofer Herlin <christofer.herlin@gmail.com>
2018-05-14 10:04:48 +0200

    feat: add Cloudinary image upload

--- screens/FinishedEventToConfirm.js
+++ screens/FinishedEventToConfirm.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, TouchableOpacity, Image } from 'react-native';
 import MapView, { Polyline } from 'react-native-maps';
+import Expo, { ImagePicker } from 'expo';
 import PropTypes from 'prop-types';
 import { confirmEvent } from '../actions/events';
 
@@ -17,17 +18,59 @@ const LATITUDE_DELTA = 0.00922;
 const LONGITUDE_DELTA = 0.00421;
 
 class FinishedEventToConfirm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+    };
+  }
+
   confirmEvent = () => {
     this.props.confirmEvent(this.props.userId, this.props.eventId, this.props.distance);
-    setTimeout(() => {
-      this.props.navigation.navigate('EventConfirmation');
-    }, 1000);
+    this.props.navigation.navigate('EventConfirmation');
   }
+
+  pickImage = async () => {
+    const { Permissions } = Expo;
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (status !== 'granted') {
+      alert('You have to allow access to your camera roll to be able to upload images. Go to your Settings and allow the app access.');
+    } else {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        mediaTypes: 'Images',
+        aspect: [4, 3],
+        base64: true,
+      });
+
+      if (!result.cancelled) {
+        this.setState({ image: result.uri });
+        const base64Img = `data:image/jpg;base64,${result.base64}`;
+        const apiUrl = 'https://api.cloudinary.com/v1_1/cherlin/image/upload';
+
+        const data = {
+          file: base64Img,
+          upload_preset: 'sux9skjp',
+        };
+
+        fetch(apiUrl, {
+          body: JSON.stringify(data),
+          headers: {
+            'content-type': 'application/json',
+          },
+          method: 'POST',
+        }).then(res => res.json())
+          .then(res => console.log(res))
+          .catch(err => console.log(err));
+      }
+    }
+  }
+
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={{ flex: 3, flexDirection: 'row' }}>
+        <View style={{ flex: 1, flexDirection: 'row' }}>
           <MapView
             region={{
               latitude:
@@ -57,6 +100,29 @@ class FinishedEventToConfirm extends React.Component {
             />
           </MapView>
         </View>
+        <TouchableOpacity onPress={this.pickImage} style={{ width: 200, alignSelf: 'center' }}>
+          <View style={{ backgroundColor: 'transparent' }}>
+            {this.state.image ?
+              <Image
+                source={{ uri: this.state.image }}
+                style={{
+                width: 200,
+                height: 200,
+                borderRadius: 100,
+                alignSelf: 'center',
+                }}
+              />
+              :
+              <View style={{
+                backgroundColor: 'grey',
+                width: 200,
+                height: 200,
+                borderRadius: 100,
+              }}
+              />
+            }
+          </View>
+        </TouchableOpacity>
         <View style={{ flex: 1 }}>
           <Button title="Cancel" onPress={() => this.props.navigation.navigate('Home')} />
           <Button title="Confirm" onPress={this.confirmEvent} />
